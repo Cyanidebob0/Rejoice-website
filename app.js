245e35046e34bc926ab79e69dea958265f1fbe9b
bhuvan <bhuvanannappa@gmail.com>
2024-09-16 20:10:36 +0530

    added a favicon

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-let cursor=document.querySelector("#cursor");
+function mouseMove() {
+    let cursor = document.querySelector("#cursor");
 
-document.querySelector("#p1-content").addEventListener("mousemove",(e)=>{
-    gsap.to(cursor,{
-        x:e.x,
-        y:e.y,
-        duration:1,
+    document.querySelector("#p1-content").addEventListener("mousemove", (e) => {
+        gsap.to(cursor, {
+            x: e.x,
+            y: e.y,
+            duration: 1,
+        })
     })
-})
 
 
-document.querySelector("#p1-content").addEventListener("mousemove",(e)=>{
-    gsap.to(cursor,{
-        x:e.x,
-        y:e.y,
-        scale:1,
-        opacity:1,
-        duration:1
+    document.querySelector("#p1-content").addEventListener("mousemove", (e) => {
+        gsap.to(cursor, {
+            x: e.x,
+            y: e.y,
+            scale: 1,
+            opacity: 1,
+            duration: 1
+        })
     })
-})
 
-document.querySelector("#p1-content").addEventListener("mouseleave",(e)=>{
-    gsap.to(cursor,{
-        x:e.x,
-        y:e.y,
-        scale:0,
-        opacity:0,
-        duration:1,
+    document.querySelector("#p1-content").addEventListener("mouseleave", (e) => {
+        gsap.to(cursor, {
+            x: e.x,
+            y: e.y,
+            scale: 0,
+            opacity: 0,
+            duration: 1,
 
+        })
     })
-})
+
+}
+mouseMove();
