aaa042623ab2dbf89ab793658c291af1f0d546e3
bhuvan <bhuvanannappa@gmail.com>
2024-09-16 15:23:04 +0530

    finished page 1 of the website

---
+++ app.js
@@ -0,0 +1,31 @@
+let cursor=document.querySelector("#cursor");
+
+document.querySelector("#p1-content").addEventListener("mousemove",(e)=>{
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        duration:1,
+    })
+})
+
+
+document.querySelector("#p1-content").addEventListener("mousemove",(e)=>{
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        scale:1,
+        opacity:1,
+        duration:1
+    })
+})
+
+document.querySelector("#p1-content").addEventListener("mouseleave",(e)=>{
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        scale:0,
+        opacity:0,
+        duration:1,
+
+    })
+})
